e7164accae0052adec4125b6041f38312c71a5d0
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2021-10-20 17:20:46 -0400

    add printf dependency with CPM

---
+++ build/CPM_modules/Findprintf.cmake
@@ -0,0 +1,3 @@
+include("/Users/haydenbraxton/Projects/cmake-example-project/cmake/CPM.cmake")
+CPMAddPackage(NAME;printf;GITHUB_REPOSITORY;mpaland/printf;VERSION;4.0.0;DONWLOAD_ONLY)
+set(printf_FOUND TRUE)
