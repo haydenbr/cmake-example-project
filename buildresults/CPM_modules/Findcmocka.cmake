f2d68102a8b192521e278247d9b4aa23d046dfbd
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2021-10-26 03:15:57 -0400

    add cmocka as external project

--- buildresults/CPM_modules/Findcmocka.cmake
+++ buildresults/CPM_modules/Findcmocka.cmake
@@ -1,3 +1,3 @@
 include("/Users/haydenbraxton/Projects/cmake-example-project/cmake/CPM.cmake")
-CPMAddPackage(NAME;cmocka;GIT_REPOSITORY;https://git.cryptomilk.org/projects/cmocka.git/;VERSION;1.1.5;GIT_TAG;cmocka-1.1.5;OPTIONS;WITH_EXAMPLES OFF;CMAKE_BUILD_TYPE DEBUG)
+CPMAddPackage(NAME;cmocka;GIT_REPOSITORY;https://git.cryptomilk.org/projects/cmocka.git/;VERSION;1.1.5;GIT_TAG;cmocka-1.1.5;DOWNLOAD_ONLY;YES)
 set(cmocka_FOUND TRUE)
