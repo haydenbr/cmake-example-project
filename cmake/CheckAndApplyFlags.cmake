cb152285e44e72350b902a2329e455e1b4134680
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2021-11-05 00:27:56 -0400

    add support for linker flags and add additional compiler flags

--- cmake/CheckAndApplyFlags.cmake
+++ cmake/CheckAndApplyFlags.cmake
@@ -1,5 +1,3 @@
-include(CheckCCompilerFlag)
-
 get_property(enabled_languages GLOBAL PROPERTY ENABLED_LANGUAGES)
 
 message("enabled_languages: ${enabled_languages}")
@@ -18,7 +16,12 @@ function(make_safe_varname input_string output_var)
 	set(${output_var} ${return_value} PARENT_SCOPE)
 endfunction()
 
+##################
+# Compiler Flags #
+##################
+
 function(apply_supported_compiler_flags lang target scope flag_list)
+	include(Check${lang}CompilerFlag)
 	#[[
 		${flag_list} this gives us the value of flag_list, which is the name of the variable provided as input
 		${${flag_list}} this gives us the value of the variable provided
@@ -37,4 +40,34 @@ endfunction()
 
 function(apply_supported_c_compiler_flags target scope flag_list)
 	apply_supported_compiler_flags("C" ${target} ${scope} ${flag_list})
+endfunction()
+
+function(apply_supported_cxx_compiler_flags target scope flag_list)
+	apply_supported_compiler_flags("CXX" ${target} ${scope} ${flag_list})
+endfunction()
+
+##################
+# Linker Flags #
+##################
+
+function(apply_supported_linker_flags lang target scope flag_list)
+	include(CheckLinkerFlag)
+	check_and_double_deref(flag_list)
+	
+	foreach(flag ${flag_list})
+		make_safe_varname(${flag} flag_var)
+		check_linker_flag(${lang} {flag} ${flag_var})
+
+		if(${flag_var})
+			target_link_options(${target} ${scope} ${flag})
+		endif()
+	endforeach()
+endfunction()
+
+function(apply_supported_c_linker_flags target scope flag_list)
+	apply_supported_linker_flags("C" ${target} ${scope} ${flag_list})
+endfunction()
+
+function(apply_supported_cxx_linker_flags target scope flag_list)
+	apply_supported_linker_flags("CXX" ${target} ${scope} ${flag_list})
 endfunction()
